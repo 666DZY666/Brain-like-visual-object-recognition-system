05effb28cd6a41a7884eac92a978b72ce96ef58c
666dzy666 <568314913@qq.com>
2019-07-16 23:27:59 +0800

    first

---
+++ 程序/README.md
@@ -0,0 +1,131 @@
+<p align="center"><img width="25%" src="docs/logo.png"/></p>
+
+A Python package used for simulating spiking neural networks (SNNs) on CPUs or GPUs using [PyTorch](http://pytorch.org/) `Tensor` functionality.
+
+BindsNET is a spiking neural network simulation library geared towards the development of biologically inspired algorithms for machine learning.
+
+This package is used as part of ongoing research on applying SNNs to machine learning (ML) and reinforcement learning (RL) problems in the [Biologically Inspired Neural & Dynamical Systems (BINDS) lab](http://binds.cs.umass.edu/).
+
+Check out the [BindsNET experiments repository](https://github.com/djsaunde/bindsnet_experiments) for a collection of experiments, accompanying bash scripts for dispatching on [CICS](https://www.cics.umass.edu/) clusters, functions for the analysis of results, plots of experiment outcomes, and more.
+
+[![Build Status](https://travis-ci.com/Hananel-Hazan/bindsnet.svg?token=trym5Uzx1rs9Ez2yENEF&branch=master)](https://travis-ci.com/Hananel-Hazan/bindsnet)
+[![Documentation Status](https://readthedocs.org/projects/bindsnet-docs/badge/?version=latest)](https://bindsnet-docs.readthedocs.io/?badge=latest)
+
+## Requirements
+
+- Python 3.6
+- `requirements.txt`
+
+## Setting things up
+
+### Using pip
+BindsNET is available on PyPI. Issue
+
+```
+pip install bindsnet
+```
+
+to get the most recent stable release. Or, to build the `bindsnet` package from source, clone the GitHub repository, change directory to the top level of this project, and issue
+
+```
+pip install .
+```
+
+Or, to install in editable mode (allows modification of package without re-installing):
+
+```
+pip install -e .
+```
+
+To install the packages necessary to interface with the [OpenAI gym RL environments library](https://github.com/openai/gym), follow their instructions for installing the packages needed to run the RL environments simulator (on Linux / MacOS).
+
+### Using Docker
+[Link](https://hub.docker.com/r/hqkhan/bindsnet/) to Docker repository.
+
+We also provide a Dockerfile in which BindsNET and all of its dependencies come installed in. Issue
+
+```
+docker image build .
+```
+at the top level directory of this project to create a docker image. 
+
+To change the name of the newly built image, issue
+```
+docker tag <IMAGE_ID> <NEW_IMAGE_ID>
+```
+
+To run a container and get a bash terminal inside it, issue
+
+```
+docker run -it <NEW_IMAGE_ID> bash
+```
+
+## Getting started
+
+To run a near-replication of the SNN from [this paper](https://www.frontiersin.org/articles/10.3389/fncom.2015.00099/full#), issue
+
+```
+cd examples/mnist
+python eth_mnist.py
+```
+
+There are a number of optional command-line arguments which can be passed in, including `--plot` (displays useful monitoring figures), `--n_neurons [int]` (number of excitatory, inhibitory neurons simulated), `--mode ['train' | 'test']` (sets network operation to the training or testing phase), and more. Run the script with the `--help` or `-h` flag for more information.
+
+A number of other examples are available in the `examples` directory that are meant to showcase BindsNET's functionality. Take a look, and let us know what you think!
+
+## Running the tests
+
+Issue the following to run the tests:
+
+```
+python -m pytest test/
+```
+
+Some tests will fail if Open AI `gym` is not installed on your machine.
+
+## Background
+
+The simulation of biologically plausible spiking neuron dynamics can be challenging. It is typically done by solving ordinary differential equations (ODEs) which describe said dynamics. PyTorch does not explicitly support the solution of differential equations (as opposed to [`brian2`](https://github.com/brian-team/brian2), for example), but we can convert the ODEs defining the dynamics into difference equations and solve them at regular, short intervals (a `dt` on the order of 1 millisecond) as an approximation. Of course, under the hood, packages like `brian2` are doing the same thing. Doing this in [`PyTorch`](http://pytorch.org/) is exciting for a few reasons:
+
+1. We can use the powerful and flexible [`torch.Tensor`](http://pytorch.org/) object, a wrapper around the [`numpy.ndarray`](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.ndarray.html) which can be transferred to and from GPU devices.
+
+2. We can avoid "reinventing the wheel" by repurposing functions from the [`torch.nn.functional`](http://pytorch.org/docs/master/nn.html#torch-nn-functional) PyTorch submodule in our SNN architectures; e.g., convolution or pooling functions.
+
+The concept that the neuron spike ordering and their relative timing encode information is a central theme in neuroscience. [Markram et al. (1997)](http://www.caam.rice.edu/~caam415/lec_gab/g4/markram_etal98.pdf) proposed that synapses between neurons should strengthen or degrade based on this relative timing, and prior to that, [Donald Hebb](https://en.wikipedia.org/wiki/Donald_O._Hebb) proposed the theory of Hebbian learning, often simply stated as "Neurons that fire together, wire together." Markram et al.'s extension of the Hebbian theory is known as spike-timing-dependent plasticity (STDP).
+
+We are interested in applying SNNs to ML and RL problems. We use STDP to modify weights of synapses connecting pairs or populations of neurons in SNNs. In the context of ML, we want to learn a setting of synapse weights which will generate data-dependent spiking activity in SNNs. This activity will allow us to subsequently perform some ML task of interest; e.g., discriminating or clustering input data. In the context of RL, we may think of the spiking neural network as an RL agent, whose spiking activity may be converted into actions in an environment's action space.
+
+We have provided some simple starter scripts for doing unsupervised learning (learning a fully-connected or convolutional representation via STDP), supervised learning (clamping output neurons to desired spiking behavior depending on data labels), and reinforcement learning (converting observations from the Atari game Space Invaders to input to an SNN, and converting network activity back to actions in the game).
+
+## Citation
+
+If you use BindsNET in your research, please cite the following [article](https://arxiv.org/abs/1806.01423):
+
+```
+@ARTICLE{2018arXiv180601423H,
+   author = {{Hazan}, H. and {Saunders}, D.~J. and {Khan}, H. and {Sanghavi}, D.~T. and 
+	{Siegelmann}, H.~T. and {Kozma}, R.},
+    title = "{BindsNET: A machine learning-oriented spiking neural networks library in Python}",
+  journal = {ArXiv e-prints},
+archivePrefix = "arXiv",
+   eprint = {1806.01423},
+ keywords = {Computer Science - Neural and Evolutionary Computing, Quantitative Biology - Neurons and Cognition},
+     year = 2018,
+    month = jun,
+   adsurl = {http://adsabs.harvard.edu/abs/2018arXiv180601423H},
+  adsnote = {Provided by the SAO/NASA Astrophysics Data System}
+}
+```
+
+## Contributors
+
+- Daniel Saunders ([email](mailto:djsaunde@cs.umass.edu))
+- Hananel Hazan ([email](mailto:hananel@hazan.org.il))
+- Darpan Sanghavi ([email](mailto:dsanghavi@cs.umass.edu))
+- Hassaan Khan ([email](mailto:hqkhan@umass.edu))
+- Devdhar Patel ([email](mailto:devdharpatel@cs.umass.edu))
+
+## License
+GNU Affero General Public License v3.0
+
+
---
+++ 程序/bindsnet/learning/__init__.py
@@ -0,0 +1,493 @@
+import torch
+import numpy as np
+
+from abc import ABC
+from typing import Union, Tuple, Optional, Sequence
+
+from ..utils import im2col_indices
+from ..network.topology import AbstractConnection, Connection, Conv2dConnection, LocallyConnectedConnection
+
+
+class LearningRule(ABC):
+    # language=rst
+    """
+    Abstract base class for learning rules.
+    """
+
+    def __init__(self, connection: AbstractConnection, nu: Optional[Union[float, Sequence[float]]] = None,
+                 weight_decay: float = 0.0) -> None:
+        # language=rst
+        """
+        Abstract constructor for the ``LearningRule`` object.
+
+        :param connection: An ``AbstractConnection`` object.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events, respectively.
+        :param weight_decay: Constant multiple to decay weights by on each iteration.
+        """
+        # Connection parameters.
+        self.connection = connection
+        self.source = connection.source
+        self.target = connection.target
+
+        self.wmin = connection.wmin
+        self.wmax = connection.wmax
+
+        # Learning rate(s).
+        if nu is None:
+            nu = 0.0
+
+        if isinstance(nu, float):
+            nu = [nu, nu]
+
+        self.nu = nu
+
+        # Weight decay.
+        self.weight_decay = weight_decay
+
+    def update(self) -> None:
+        # language=rst
+        """
+        Abstract method for a learning rule update.
+        """
+        # Implement weight decay.
+        if self.weight_decay:
+            self.connection.w -= self.weight_decay * self.connection.w
+
+        # Bound weights.
+        if None not in [self.connection.wmin, self.connection.wmax] and not isinstance(self, NoOp):
+            self.connection.w = torch.clamp(
+                self.connection.w, self.connection.wmin, self.connection.wmax
+            )
+
+
+class NoOp(LearningRule):
+    # language=rst
+    """
+    Learning rule with no effect.
+    """
+
+    def __init__(self, connection: AbstractConnection, nu: Optional[Union[float, Sequence[float]]] = None,
+                 weight_decay: float = 0.0) -> None:
+        # language=rst
+        """
+        Abstract constructor for the ``LearningRule`` object.
+
+        :param connection: An ``AbstractConnection`` object which this learning rule will have no effect on.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events, respectively.
+        :param weight_decay: Constant multiple to decay weights by on each iteration.
+        """
+        super().__init__(
+            connection=connection, nu=nu, weight_decay=weight_decay
+        )
+
+    def update(self, **kwargs) -> None:
+        # language=rst
+        """
+        Abstract method for a learning rule update.
+        """
+        super().update()
+
+
+class PostPre(LearningRule):
+    # language=rst
+    """
+    Simple STDP rule involving both pre- and post-synaptic spiking activity. The pre-synaptic update is negative, while
+    the post-synpatic update is positive.
+    """
+
+    def __init__(self, connection: AbstractConnection, nu: Optional[Union[float, Sequence[float]]] = None,
+                 weight_decay: float = 0.0) -> None:
+        # language=rst
+        """
+        Constructor for ``PostPre`` learning rule.
+
+        :param connection: An ``AbstractConnection`` object whose weights the ``PostPre`` learning rule will modify.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events, respectively.
+        :param weight_decay: Constant multiple to decay weights by on each iteration.
+        """
+        super().__init__(
+            connection=connection, nu=nu, weight_decay=weight_decay
+        )
+
+        assert self.source.traces and self.target.traces, 'Both pre- and post-synaptic nodes must record spike traces.'
+
+        if isinstance(connection, (Connection, LocallyConnectedConnection)):
+            self.update = self._connection_update
+        elif isinstance(connection, Conv2dConnection):
+            self.update = self._conv2d_connection_update
+        else:
+            raise NotImplementedError(
+                'This learning rule is not supported for this Connection type.'
+            )
+
+    def _connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        Post-pre learning rule for ``Connection`` subclass of ``AbstractConnection`` class.
+        """
+        super().update()
+
+        source_s = self.source.s.view(-1).float()
+        source_x = self.source.x.view(-1)
+        target_s = self.target.s.view(-1).float()
+        target_x = self.target.x.view(-1)
+
+        shape = self.connection.w.shape
+        self.connection.w = self.connection.w.view(self.source.n, self.target.n)
+
+        # Pre-synaptic update.
+        self.connection.w -= self.nu[0] * torch.ger(
+            source_s, target_x
+        )
+        # Post-synaptic update.
+        self.connection.w += self.nu[1] * torch.ger(
+            source_x, target_s
+        )
+
+        self.connection.w = self.connection.w.view(*shape)
+
+    def _conv2d_connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        Post-pre learning rule for ``Conv2dConnection`` subclass of ``AbstractConnection`` class.
+        """
+        super().update()
+
+        # Get convolutional layer parameters.
+        out_channels, _, kernel_height, kernel_width = self.connection.w.size()
+        padding, stride = self.connection.padding, self.connection.stride
+
+        # Reshaping spike traces and spike occurrences.
+        x_source = im2col_indices(
+            self.source.x, kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        x_target = self.target.x.permute(1, 2, 3, 0).reshape(out_channels, -1)
+        s_source = im2col_indices(
+            self.source.s.float(), kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        s_target = self.target.s.permute(1, 2, 3, 0).reshape(out_channels, -1).float()
+
+        # Pre-synaptic update.
+        pre = x_target @ s_source.t()
+        self.connection.w -= self.nu[0] * pre.view(self.connection.w.size())
+
+        # Post-synaptic update.
+        post = s_target @ x_source.t()
+        self.connection.w += self.nu[1] * post.view(self.connection.w.size())
+
+
+class Hebbian(LearningRule):
+    # language=rst
+    """
+    Simple Hebbian learning rule. Pre- and post-synaptic updates are both positive.
+    """
+
+    def __init__(self, connection: AbstractConnection, nu: Optional[Union[float, Sequence[float]]] = None,
+                 weight_decay: float = 0.0) -> None:
+        # language=rst
+        """
+        Constructor for ``Hebbian`` learning rule.
+
+        :param connection: An ``AbstractConnection`` object whose weights the ``Hebbian`` learning rule will modify.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events, respectively.
+        :param weight_decay: Constant multiple to decay weights by on each iteration.
+        """
+        super().__init__(
+            connection=connection, nu=nu, weight_decay=weight_decay
+        )
+
+        assert self.source.traces and self.target.traces, 'Both pre- and post-synaptic nodes must record spike traces.'
+
+        if isinstance(connection, (Connection, LocallyConnectedConnection)):
+            self.update = self._connection_update
+        elif isinstance(connection, Conv2dConnection):
+            self.update = self._conv2d_connection_update
+        else:
+            raise NotImplementedError(
+                'This learning rule is not supported for this Connection type.'
+            )
+
+    def _connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        Hebbian learning rule for ``Connection`` subclass of ``AbstractConnection`` class.
+        """
+        super().update()
+
+        source_s = self.source.s.view(-1).float()
+        source_x = self.source.x.view(-1)
+        target_s = self.target.s.view(-1).float()
+        target_x = self.target.x.view(-1)
+
+        shape = self.connection.w.shape
+        self.connection.w = self.connection.w.view(self.source.n, self.target.n)
+
+        # Pre-synaptic update.
+        self.connection.w += self.nu[0] * torch.ger(
+            source_s, target_x
+        )
+        # Post-synaptic update.
+        self.connection.w += self.nu[1] * torch.ger(
+            source_x, target_s
+        )
+
+        self.connection.w = self.connection.w.view(*shape)
+
+    def _conv2d_connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        Hebbian learning rule for ``Conv2dConnection`` subclass of ``AbstractConnection`` class.
+        """
+        super().update()
+
+        out_channels, _, kernel_height, kernel_width = self.connection.w.size()
+        padding, stride = self.connection.padding, self.connection.stride
+
+        # Reshaping spike traces and spike occurrences.
+        x_source = im2col_indices(
+            self.source.x, kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        x_target = self.target.x.permute(1, 2, 3, 0).reshape(out_channels, -1)
+        s_source = im2col_indices(
+            self.source.s.float(), kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        s_target = self.target.s.permute(1, 2, 3, 0).reshape(out_channels, -1).float()
+
+        # Pre-synaptic update.
+        pre = x_target @ s_source.t()
+        self.connection.w += self.nu[0] * pre.view(self.connection.w.size())
+
+        # Post-synaptic update.
+        post = s_target @ x_source.t()
+        self.connection.w += self.nu[1] * post.view(self.connection.w.size())
+
+
+class MSTDP(LearningRule):
+    # language=rst
+    """
+    Reward-modulated STDP. Adapted from `(Florian 2007) <https://florian.io/papers/2007_Florian_Modulated_STDP.pdf>`_.
+    """
+
+    def __init__(self, connection: AbstractConnection, nu: Optional[Union[float, Sequence[float]]] = None,
+                 weight_decay: float = 0.0) -> None:
+        # language=rst
+        """
+        Constructor for ``MSTDP`` learning rule.
+
+        :param connection: An ``AbstractConnection`` object whose weights the ``MSTDP`` learning rule will modify.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events, respectively.
+        """
+        super().__init__(
+            connection=connection, nu=nu, weight_decay=weight_decay
+        )
+
+        assert self.source.traces and self.target.traces, 'Both pre- and post-synaptic nodes must record spike traces.'
+
+        if isinstance(connection, (Connection, LocallyConnectedConnection)):
+            self.update = self._connection_update
+        elif isinstance(connection, Conv2dConnection):
+            self.update = self._conv2d_connection_update
+        else:
+            raise NotImplementedError(
+                'This learning rule is not supported for this Connection type.'
+            )
+
+    def _connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        M-STDP learning rule for ``Connection`` subclass of ``AbstractConnection`` class.
+
+        Keyword arguments:
+
+        :param float reward: Reward signal from reinforcement learning task.
+        :param float a_plus: Learning rate (post-synaptic).
+        :param float a_minus: Learning rate (pre-synaptic).
+        """
+        super().update()
+
+        source_s = self.source.s.view(-1).float()
+        source_x = self.source.x.view(-1)
+        target_s = self.target.s.view(-1).float()
+        target_x = self.target.x.view(-1)
+
+        shape = self.connection.w.shape
+        self.connection.w = self.connection.w.view(self.source.n, self.target.n)
+
+        # Parse keyword arguments.
+        reward = kwargs['reward']
+        a_plus = kwargs.get('a_plus', 1)
+        a_minus = kwargs.get('a_minus', -1)
+
+        # Get P^+ and P^- values (function of firing traces).
+        p_plus = a_plus * source_x
+        p_minus = a_minus * target_x
+
+        # Calculate point eligibility value.
+        eligibility = torch.ger(p_plus, target_s) + torch.ger(source_s, p_minus)
+
+        # Compute weight update.
+        self.connection.w += self.nu[0] * reward * eligibility
+        self.connection.w = self.connection.w.view(*shape)
+
+    def _conv2d_connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        M-STDP learning rule for ``Conv2dConnection`` subclass of ``AbstractConnection`` class.
+
+        Keyword arguments:
+
+        :param float reward: Reward signal from reinforcement learning task.
+        :param float a_plus: Learning rate (post-synaptic).
+        :param float a_minus: Learning rate (pre-synaptic).
+        """
+        super().update()
+
+        # Parse keyword arguments.
+        reward = kwargs['reward']
+        a_plus = kwargs.get('a_plus', 1)
+        a_minus = kwargs.get('a_minus', -1)
+
+        out_channels, _, kernel_height, kernel_width = self.connection.w.size()
+        padding, stride = self.connection.padding, self.connection.stride
+
+        # Reshaping spike traces and spike occurrences.
+        x_source = im2col_indices(
+            self.source.x, kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        x_target = self.target.x.permute(1, 2, 3, 0).reshape(out_channels, -1)
+        s_source = im2col_indices(
+            self.source.s.float(), kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        s_target = self.target.s.permute(1, 2, 3, 0).reshape(out_channels, -1).float()
+
+        # Get P^+ and P^- values (function of firing traces), and reshape source and target spikes.
+        p_plus = a_plus * x_source
+        p_minus = a_minus * x_target
+
+        # Pre- and post-synaptic updates.
+        pre = (s_source @ p_minus.t()).view(self.connection.w.size())
+        post = (p_plus @ s_target.t()).view(self.connection.w.size())
+
+        # Calculate point eligibility value.
+        eligibility = post + pre
+
+        # Compute weight update.
+        self.connection.w += self.nu[0] * reward * eligibility
+
+
+class MSTDPET(LearningRule):
+    # language=rst
+    """
+    Reward-modulated STDP with eligibility trace. Adapted from
+    `(Florian 2007) <https://florian.io/papers/2007_Florian_Modulated_STDP.pdf>`_.
+    """
+
+    def __init__(self, connection: AbstractConnection, nu: Optional[Union[float, Sequence[float]]] = None,
+                 weight_decay: float = 0.0) -> None:
+        # language=rst
+        """
+        Constructor for ``MSTDPET`` learning rule.
+
+        :param connection: An ``AbstractConnection`` object whose weights the ``MSTDPET`` learning rule will modify.
+        :param nu: Single or pair of learning rates for pre- and post-synaptic events, respectively.
+        :param weight_decay: Constant multiple to decay weights by on each iteration.
+        """
+        super().__init__(
+            connection=connection, nu=nu, weight_decay=weight_decay
+        )
+
+        assert self.source.traces and self.target.traces, 'Both pre- and post-synaptic nodes must record spike traces.'
+
+        if isinstance(connection, (Connection, LocallyConnectedConnection)):
+            self.update = self._connection_update
+        elif isinstance(connection, Conv2dConnection):
+            self.update = self._conv2d_connection_update
+        else:
+            raise NotImplementedError(
+                'This learning rule is not supported for this Connection type.'
+            )
+
+        self.e_trace = torch.zeros(self.source.n, self.target.n)
+        self.tc_e_trace = 0.04
+        self.p_plus = torch.zeros(self.source.n)
+        self.tc_plus = 0.05
+        self.p_minus = torch.zeros(self.target.n)
+        self.tc_minus = 0.05
+
+    def _connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        M-STDP-ET learning rule for ``Connection`` subclass of ``AbstractConnection`` class.
+
+        Keyword arguments:
+
+        :param float reward: Reward signal from reinforcement learning task.
+        :param float a_plus: Learning rate (post-synaptic).
+        :param float a_minus: Learning rate (pre-synaptic).
+        """
+        super().update()
+
+        source_s = self.source.s.view(-1).float()
+        source_x = self.source.x.view(-1)
+        target_s = self.target.s.view(-1).float()
+        target_x = self.target.x.view(-1)
+
+        # Parse keyword arguments.
+        reward = kwargs['reward']
+        a_plus = kwargs.get('a_plus', 1)
+        a_minus = kwargs.get('a_minus', -1)
+
+        # Get P^+ and P^- values (function of firing traces).
+        self.p_plus = self.p_plus * np.exp(-self.connection.dt * self.tc_plus) + a_plus * source_x
+        self.p_minus = self.p_minus * np.exp(-self.connection.dt * self.tc_minus) + a_minus * target_x
+
+        # Calculate value of eligibility trace.
+        self.e_trace += torch.ger(self.p_plus, target_s) + torch.ger(source_s, self.p_minus)
+
+        # Compute weight update.
+        self.connection.w += self.nu[0] * reward * self.e_trace
+
+    def _conv2d_connection_update(self, **kwargs) -> None:
+        # language=rst
+        """
+        M-STDP-ET learning rule for ``Conv2dConnection`` subclass of ``AbstractConnection`` class.
+
+        Keyword arguments:
+
+        :param float reward: Reward signal from reinforcement learning task.
+        :param float a_plus: Learning rate (post-synaptic).
+        :param float a_minus: Learning rate (pre-synaptic).
+        """
+        super().update()
+
+        # Parse keyword arguments.
+        reward = kwargs['reward']
+        a_plus = kwargs.get('a_plus', 1)
+        a_minus = kwargs.get('a_minus', -1)
+
+        out_channels, _, kernel_height, kernel_width = self.connection.w.size()
+        padding, stride = self.connection.padding, self.connection.stride
+
+        # Reshaping spike traces and spike occurrences.
+        x_source = im2col_indices(
+            self.source.x, kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        x_target = self.target.x.permute(1, 2, 3, 0).reshape(out_channels, -1)
+        s_source = im2col_indices(
+            self.source.s.float(), kernel_height, kernel_width, padding=padding, stride=stride
+        )
+        s_target = self.target.s.permute(1, 2, 3, 0).reshape(out_channels, -1).float()
+
+        # Get P^+ and P^- values (function of firing traces).
+        self.p_plus = self.p_plus * np.exp(-self.network.dt / self.tc_plus) + a_plus * x_source
+        self.p_minus = self.p_minus * np.exp(-self.network.dt / self.tc_minus) + a_minus * x_target
+
+        # Post-synaptic and pre-synaptic updates.
+        post = (self.p_plus @ s_target.t()).view(self.connection.w.size())
+        pre = (s_source @ self.p_minus.t()).view(self.connection.w.size())
+
+        # Calculate value of eligibility trace.
+        self.e_trace = post + pre
+
+        # Compute weight update.
+        self.connection.w += self.nu[0] * reward * self.e_trace
